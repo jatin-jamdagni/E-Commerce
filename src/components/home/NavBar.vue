049c1a6c9e702be974f55ef65d50d0acbb0d48bf
Jatin Sharma <jatinsharma13arril2002@gmail.com>
2023-11-29 21:13:54 +0530

    added

--- src/components/home/NavBar.vue
+++ src/components/home/NavBar.vue
@@ -17,22 +17,25 @@
     <div class="flex justify-evenly space-x-6 bg-transparent">
       <div class="flex items-center relative bg-transparent">
         <input
+          id="search"
           type="text"
           placeholder="Search.."
-          class="bg-[#202327] py-2 px-4 pl-12 rounded-lg w-80 text-[#6B240C] font-medium placeholder:text-[#F5CCA0]"
+          class="bg-[#202327] py-2 px-4 pl-12 rounded-lg w-[60vw] text-[#6B240C] font-medium placeholder:text-[#F5CCA0]"
         />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="16"
-          width="16"
-          viewBox="0 0 512 512"
-          class="absolute left-4 bg-transparent"
-          fill="#6B240C"
-        >
-          <path
-            d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
-          />
-        </svg>
+        <label for="search" class="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="16"
+            width="16"
+            viewBox="0 0 512 512"
+            class="absolute left-4 bg-transparent"
+            fill="#6B240C"
+          >
+            <path
+              d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+            />
+          </svg>
+        </label>
       </div>
       <button @click="router.push('/cart')">
         <svg
